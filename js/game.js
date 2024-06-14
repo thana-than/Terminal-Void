0235544ca6e7dc3f070cf1a6b93264037d94812a
Than <thanthemannn@gmail.com>
2024-06-14 01:56:04 -0400

    feat: basic navigation (cd and ls) build type is parsed from json

--- js/game.js
+++ js/game.js
@@ -1,70 +1,141 @@
 const commandHistory = [];
 let historyIndex = -1;
-let directories = {}; // Object to store the directory structure
+let fileSystem;
+let currentDirectory;
+let build;
+const REGEX_SLASH = /\/|\\/
 
-// Function to initialize the directory structure
-function initializeDirectories() {
-    // Assuming your directory structure is provided in a JSON format
-    // You can adjust this part based on how your directory structure is stored
-    const directoryStructure = {
-        "root": {
-            "folder1": {
-                "file1.html": "<h1>This is file1.html in folder1</h1>",
-                "file2.html": "<h1>This is file2.html in folder1</h1>"
-            },
-            "folder2": {
-                "file3.html": "<h1>This is file3.html in folder2</h1>"
-            }
-        }
-    };
+//TODO clean this shit up (move to other files)
 
-    // // Recursively build the directory structure
-    // buildDirectoryStructure(directoryStructure, "");
+async function initialize() {
+    //*Load json
+    const response = await fetch('fileStructure.json');
+    const json = await response.json()
+
+    //*Get build type
+    build = json["build"]
+
+    //* Build file structure
+    fileSystem = buildNodes(json["file_structure"], null);
+    fileSystem.name = "ROOT" //*We rename the gameFolder to ROOT for gameplay
+    currentDirectory = fileSystem;
+
+    log(`Initialized virtual file system.\nCurrent Directory: ${currentDirectory.name}`);
+    log(fileSystem);
 }
 
-// Function to recursively build the directory structure
-// function buildDirectoryStructure(structure, currentPath) {
-//     for (const [key, value] of Object.entries(structure)) {
-//         const fullPath = currentPath === "" ? key : `${currentPath}/${key}`;
-//         directories[fullPath] = value;
-//         if (typeof value === "object") {
-//             buildDirectoryStructure(value, fullPath);
-//         }
-//     }
-// }
+function log(logText) {
+    if (build != "RELEASE")
+        console.log(logText)
+}
+
+function buildNodes(jsonNode, parent) {
+    let node = new PathNode(jsonNode, parent)
+
+    if (node.isFile)
+        return node;
+
+    jsonNode["children"].forEach(child => {
+        let cNode = buildNodes(child, node)
+        node.children.set(cNode.name.toLowerCase(), cNode)
+    });
+
+    return node
+}
+
+class PathNode {
+    constructor(jsonNode, parent) {
+        this.name = jsonNode["name"];
+        this.pathLink = jsonNode["pathLink"]
+        this.type = jsonNode["type"]
+        this.isFile = this.type != "directory"
+        this.parent = parent
+        if (!this.isFile)
+            this.children = new Map();
+    }
+
+    toPath() {
+        if (this.parent == null)
+            return this.name;
+
+        return `${this.parent.toPath()}/${this.name}`
+    }
+}
+
+function toNode(path) {
+    //* If the path starts with a slash, we start at root. If not, this is a local path
+    let currentNode = REGEX_SLASH.test(path.charAt(0)) ? fileSystem : currentDirectory;
+
+    //* Splits the path up so we can work through each part
+    const pathArray = path.split(REGEX_SLASH)
+    const len = pathArray.length;
+
+    for (let i = 0; i < len; i++) {
+        next = pathArray[i];
+
+        let nextNode = currentNode
+
+        //* ".." means go up one directory
+        if (next == "..") {
+            if (currentNode.parent)
+                nextNode = currentNode.parent;
+            else return currentNode; //* If theres no parent, we're at the root, just return this!
+        }
+        //* Test for a child in our current node
+        //* Ignore dots or blanks, as that keeps us on the current node
+        else if (next != "." && next != '') {
+            nextNode = currentNode.children.get(next);
+        }
+
+        //* If at any point we lose our node (or the node is a file), that means the path is invalid
+        if (!nextNode || nextNode.isFile)
+            return null;
+
+        currentNode = nextNode
+    }
+
+    return currentNode;
+}
 
 // Function to change directory
 function changeDirectory(dirName) {
-    const newPath = currentDirectory === "" ? dirName : `${currentDirectory}/${dirName}`;
-    if (directories[newPath]) {
-        currentDirectory = newPath;
-        return '';
-    } else {
-        return `Directory '${dirName}' not found.`;
-    }
+    let n = toNode(dirName)
+    if (!n) return `Path '${dirName}' is not a directory.`;
+
+    currentDirectory = n;
+    return n.toPath();
 }
 
 // Function to list contents of the current directory
 function listContents() {
-    const contents = Object.keys(directories[currentDirectory] || {});
+    const contents = Array.from(currentDirectory.children.values(), (n) => n.name);
     return contents.join('<br>');
 }
 
-// Function to display command response
-function displayResponse(command, response) {
+function print(text) {
     const output = document.getElementById('output');
     const div = document.createElement('div');
     div.classList.add('content');
-    div.innerHTML = `<div class='command'>> ${command}</div><div class='response'>${response}</div>`;
+    div.innerHTML = text;
     output.appendChild(div);
     output.scrollTop = output.scrollHeight;
 }
 
-function sendCommand(command) {
-    displayResponse(command, "");
+
+function printCommand(command, response) {
+    print(`<div class='command'>> ${command}</div><div class='response'>${response}</div>`);
 }
 
-let currentDirectory = '';
+function sendCommand(command) {
+    let response;
+    //TODO command parsing
+    const cleaned_command = command.toLowerCase().trim()
+    if (cleaned_command == "ls")
+        response = listContents();
+    else
+        response = changeDirectory(cleaned_command);
+    printCommand(command, response);
+}
 
 document.getElementById('input').addEventListener('keydown', async function (event) {
     const inputElement = event.target;
@@ -94,4 +165,4 @@ document.getElementById('input').addEventListener('keydown', async function (eve
 });
 
 // Initialize the directory structure when the page loads
-initializeDirectories();
+initialize();
