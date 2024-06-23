b379fcf25500db09f98f423d922fadbdcac492f5
Than <thanthemannn@gmail.com>
2024-06-23 15:48:38 -0400

    feat: read text file!

--- src/js/fileHandle.js
+++ src/js/fileHandle.js
@@ -1,6 +1,5 @@
-import { gameComponents } from '../.generated/dynamicImports';
-
-const delay = ms => new Promise(res => setTimeout(res, ms));
+import { gameFiles } from '../.generated/dynamicImports';
+import { Program } from './program.js'
 
 class FileHandle {
     static fileHandles = new Map();
@@ -8,13 +7,19 @@ class FileHandle {
     static async open(node) {
         const handle = this.fileHandles.get(node.type)
         if (!handle) {
-            console.log(`File '${node.pathLink}' is not a recognized type.`) //TODO console.log should be replaced so it doesn't show up in build
+            Program.log(`File '${node.pathLink}' is not a recognized type.`)
             return `File '${node.fullName}' is not a recognized type.`;
         }
 
-        console.log(`Opening ${node.fullName} at ${node.pathLink}`);  //TODO console.log should be replaced so it doesn't show up in build
-        //TODO load file first then pass
-        return handle.read(node);
+        Program.log(`Opening ${node.fullName} at ${node.pathLink}`);
+
+        const file = await load(node.hash);
+        if (file) {
+            Program.log(`Reading file at path ${node.pathLink}.`)
+            return handle.read(node, file);
+        }
+
+        return null;
     }
 
     constructor(extensions) {
@@ -22,6 +27,24 @@ class FileHandle {
         this.extensions.forEach(key => {
             FileHandle.fileHandles.set(key, this);
         });
+    }
+}
+
+function load(hash) {
+    const importComponent = gameFiles[hash];
+
+    if (importComponent) {
+        return importComponent()
+            .then((module) => {
+                return module.default || module;
+            })
+            .catch((error) => {
+                console.error(`Error loading file with hash ${hash}:`, error);
+                throw error;
+            });
+    } else {
+        console.error(`File with hash: ${hash} not found`);
+        return Promise.reject(new Error(`File with hash: ${hash} not found`));
     }
 }
 
@@ -41,15 +64,15 @@ function fileHandleFactory(properties) {
 
 const TextHandle = fileHandleFactory({
     extensions: ['txt'],
-    read: function (file) {
-        return `Reading file ${file.pathLink}.`;
+    read: function (node, file) {
+        return file;
     }
 })
 
 const JSHandle = fileHandleFactory({
     extensions: ['js'],
-    read: async function (file) {
-        return `Reading file ${file.pathLink}.`;
+    read: async function (node, file) {
+        return `Reading file ${node.pathLink}.`;
     }
 })
 
